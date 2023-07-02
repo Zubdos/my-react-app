fff4861bbd20597fb44964f2a016529b81fc31ef
Zubdos <azunrise@gmail.com>
2023-07-03 01:19:25 +0300

    https://github.com/Zubdos/react-app.git

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,27 @@
+import './index.css';
+
+
+export function Footer() {
+    return <footer className='footer'>
+        <div className='container'>
+            <div className='footer__wrapper'>              
+                <div className='footer__col'>
+                    Лого
+                    <p className='footer__opyright'>Интернет-магазин</p>
+                </div>
+                <div className='footer__col'>
+                    <nav className='menu-bottom'>
+                        <a href='/'className='menu-bottom__item'>Catalog</a>
+                        <a href='/'className='menu-bottom__item'>Actions</a>
+                        <a href='/'className='menu-bottom__item'>News</a>
+                        <a href='/'className='menu-bottom__item'>Reviews</a>
+
+                    </nav>
+                </div>   
+                <div className='footer__col'>
+                <div className='footer__col'>
+            
+            </div>    
+        </div>
+    </footer>
+  }
